e06b4f4a3b41a8095f5fe4ef2e78faab1c6ebab7
andreas <andreasmouzong@gmail.com>
2020-11-13 09:13:50 +0100

    initial Commit added the home component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <h1>Let's build an AirBnb clone with reatjs 🚀 </h1>
+
+     { /* Header Component */ } 
+
+          { /**/ }
+
+     { /* Banner Component */ }
+
+     { /* Card Component */ }
+
+     { /* Footer Component */ }
+
     </div>
   );
 }
